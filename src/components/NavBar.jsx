a5147e275ef6eb6450b6858ae478eccc4cc49581
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-07-04 22:53:29 +0200

    dropdown navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,49 @@
 import { Link } from "react-router-dom"
+import { RiCloseLine, RiMenuLine } from "react-icons/ri"
+import { useState } from "react"
 import "./NavBar.css"
 
 export const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
+
+  const closeMenu = () => {
+    setShowMenu(false)
+  }
+
   return (
     <>
       {/* Phone */}
+      <div className="navsection-phone">
+        <div onClick={toggleMenu}>
+          {showMenu ? <RiCloseLine /> : <RiMenuLine />}
+        </div>
+        <div className={`dropdown-menu ${showMenu ? "open" : ""}`}>
+        <ul>
+            <li>
+              <Link to={"/butik"} onClick={closeMenu}>Butiken</Link>
+            </li>
+            <li>
+              <Link to={"/flyglar"} onClick={closeMenu}>Flyglar</Link>
+            </li>
+            <li>
+              <Link to={"/piano"} onClick={closeMenu}>Pianon</Link>
+            </li>
+            <li>
+              <Link to={"/kontakt"} onClick={closeMenu}>Kontakt</Link>
+            </li>
+            <li>
+              <Link to={"/kopvillkor"} onClick={closeMenu}>Köpvillkor</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
 
       {/* Tablet */}
-      <div className="navbar">
+      <div className="navsection-tablet">
         <div>
           <Link to="/">PianoMäster</Link>
         </div>
