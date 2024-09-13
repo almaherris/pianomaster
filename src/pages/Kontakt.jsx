a0fe545f7c8024d1f5e1aa69ec9da21dff6eb570
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-09-13 12:28:54 +0200

    add contact section

--- src/pages/Kontakt.jsx
+++ src/pages/Kontakt.jsx
@@ -9,10 +9,13 @@ export const Kontakt = () => {
   return (
     <div>
       <h2>Kontakt</h2>
-      <p>Kombinerad kontakt och om oss</p>
       <p>
-        Vilka jobbar? Hur länge har de jobbat i branchen? Certifikat och att de
-        är auktoriserade pianoförsäljare etc.
+        Jag heter Linus och har över 10 års erfarenhet av att arbeta med
+        instrument, särskilt flyglar och pianon. Genom mitt arbete strävar jag
+        efter att bevara och förbättra instrumentens spelglädje och livslängd,
+        oavsett om det handlar om stämning, service eller renovering. Har du
+        frågor eller funderingar, tveka inte att kontakta mig – jag hjälper
+        gärna till!
       </p>
       <MapSection />
     </div>
